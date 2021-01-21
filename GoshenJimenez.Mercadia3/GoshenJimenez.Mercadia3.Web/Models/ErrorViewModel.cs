d04cb6b87bfa30cb6a03358d2d636618878dc0d9
Goshen Jimenez <goshen.jimenez@obelisksecurity.com>
2021-01-21 13:04:56 +0800

    Creating Project, and sample views

---
+++ GoshenJimenez.Mercadia3/GoshenJimenez.Mercadia3.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoshenJimenez.Mercadia3.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
