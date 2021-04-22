3e399223e2e8e9a51f3c6ff432db2b18f9d961b0
Goshen Jimenez <goshen.jimenez@obelisksecurity.com>
2021-04-22 10:06:58 +0800

    Change Password

---
+++ GoshenJimenez.Mercadia3/GoshenJimenez.Mercadia3.Web/ViewModels/Account/ChangePasswordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoshenJimenez.Mercadia3.Web.ViewModels.Account
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        public string OldPassword { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+        
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+}
